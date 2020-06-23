462fc3a4e25eec5ec1aa1390fa420526964b243f
QuantumMaid Developer <developer@quantummaid.de>
2020-06-23 13:26:04 +0300

    added 1.5s test on first invocation

--- aws-lambda/itests/src/test/scripts/httpapi-tests.sh
+++ aws-lambda/itests/src/test/scripts/httpapi-tests.sh
@@ -3,7 +3,12 @@
 my_dir="$(dirname "$(readlink -e "$0")")"
 source "${my_dir}/.envrc"
 
-oneTimeSetUp() {
+function progress() {
+    local msg="$1" && shift
+    printf "\n==> $msg" "$@"
+}
+
+function oneTimeSetUp() {
     readonly local api_id=$(aws cloudformation describe-stack-resource \
       --stack-name "${lambda_stack_name}" \
       --logical-resource-id ServerlessHttpApi \
@@ -14,21 +19,33 @@ oneTimeSetUp() {
     readonly api_url=$(printf "https://%s.execute-api.%s.amazonaws.com" "${api_id}" "${region}")
 }
 
-testHttpApiFirstInvocationLastsNoLongerThanOnePointFiveSeconds() {
-    TIMEFORMAT='%3R' invokeApi "/helloworld" 'Hello world!'
+function testFirstInvocationLastsNoLongerThanOnePointFiveSeconds() {
+    readonly local before=$(currentTimeMillis)
+    invokeApi "/helloworld" 'Hello world!'
+    readonly local after=$(currentTimeMillis)
+    declare -i elapsed_time_millis=$after-$before
+    progress "first invocation took %sms" $elapsed_time_millis
+    assertTrue "invoking the api for the first time takes less than 1.5s" "[ $elapsed_time_millis -lt 1500 ]"
+    sleepOneSecondToEnsureXrayTracingWorksOnNextRequest
+}
+
+function currentTimeMillis() {
+    date +%s%N | cut -b1-13
+}
+
+function invokeApi() {
+    readonly local path="$1"
+    readonly local expect_response="$2"
+    readonly local curl_cmd="curl -Ss \"${api_url}${path}\""
+    progress "${curl_cmd}"
+    readonly local actual_response=$(eval "${curl_cmd}")
+    assertEquals "GET ${api_url}${path} responds with '${expect_response}'" "${expect_response}" "${actual_response}"
 }
 
 # https://docs.aws.amazon.com/lambda/latest/dg/services-xray.html
 # "The default sampling rule is 1 request per second and 5 percent of additional requests."
-sleepOneSecondToEnsureXrayTracingWorksOnNextRequest() {
+function sleepOneSecondToEnsureXrayTracingWorksOnNextRequest() {
     sleep 1
-}
-
-invokeApi() {
-    readonly local path="$1"
-    readonly local expect_response="$2"
-    readonly local actual_response=$(curl -Ss "${api_url}${path}")
-    assertEquals "GET ${api_url}${path} responds with '${expect_response}'" "${expect_response}" "${actual_response}"
 }
 
 # Load shUnit2.
